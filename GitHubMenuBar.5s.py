7dc6d36c3295b6705c5c2cedf49923bc33078f0f
Jared Lorince <jlorince@narrativescience.com>
2019-06-14 15:13:28 -0500

    wip

---
+++ GitHubMenuBar.5s.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env PYTHONIOENCODING=UTF-8 /Users/jlorince/anaconda3/bin/python
+from dotenv import load_dotenv
+# Need to load the env before importing `CONFIG`
+load_dotenv()
+import os
+import subprocess
+import sys
+from github_menubar import BitBarRenderer, CONFIG
+import psutil
+
+
+PID_FILE = CONFIG["pid_file"]
+
+
+def startProcess():
+    process = subprocess.Popen(
+        [sys.executable.replace("python", "gmb-server")], env=os.environ.copy()
+    )
+    # Write PID file
+    with open(PID_FILE, "w") as fi:
+        fi.write(str(process.pid))
+
+
+if __name__ == "__main__":
+
+    if os.path.exists(PID_FILE):
+        with open(PID_FILE, "r") as fi:
+            pid = int(fi.read().strip())
+        if psutil.pid_exists(pid):
+            renderer = BitBarRenderer(pid)
+            renderer.print_state()
+        else:
+            startProcess()
+            sys.exit()
+    else:
+        startProcess()
+        sys.exit()
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='github_menubar',
-    version='0.1',
-    description='GitHubMenubar',
-    url='http://github.com/storborg/funniest',
-    author='Jared Lorince',
-    author_email='jlorince@narrativescience.com',
-    license='MIT',
-    packages=['github_menubar'],
+    name="github_menubar",
+    version="0.1",
+    description="GitHubMenubar",
+    url="https://github.com/jlorince/GitHubMenuBar",
+    author="Jared Lorince",
+    author_email="jlorince@narrativescience.com",
+    license="MIT",
+    packages=["github_menubar"],
     install_requites=[
         "github3",
         "tabulate",
@@ -17,9 +17,8 @@ setup(
         "python-dotenv",
         "flask",
         "requests",
-        "apscheduler"
+        "apscheduler",
     ],
-    entry_points={
-        'console_scripts': ['gmb-server=github_menubar.server:main'],
-    },
-    zip_safe=False)
+    entry_points={"console_scripts": ["gmb-server=github_menubar.server:main"]},
+    zip_safe=False,
+)
